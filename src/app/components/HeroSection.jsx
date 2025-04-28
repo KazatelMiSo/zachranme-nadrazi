f8e28a0dd6984fe66e5a6dbd3beb3fe246738cd8
MichalŠoltys <166061256+KazatelMiSo@users.noreply.github.com>
2025-04-28 14:45:19 +0200

    HeroSection

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section className="flex items-center justify-center min-h-screen bg-yellow-100 text-center">
+      <h2 className="text-4xl font-bold text-yellow-900">
+        Vítejte na stránkách záchrany Karvinského nádraží!
+      </h2>
+    </section>
+  );
+};
+
+export default HeroSection;
